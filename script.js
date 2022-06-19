417091ecc5382cd46fcf3a0ceef6bd12b56105b0
ariskarini <iskariniariska@gmail.com>
2022-06-19 16:42:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+$.getJSON("https://api-alquranid.herokuapp.com/surah", function (value) {
+  //   console.log(value);
+
+  $.each(value.data, function (i, value) {
+    $("#list-surah").append(
+      '<div class="col-lg-2 col-md-3 col-6 mb-4"><div class="card"><div class="card-body"><h6><a onClick="sessionStorage.somekey = ' +
+        value.nomor +
+        '; console.log(sessionStorage.somekey);" data-fajax="false" href="isi-ayat.html" data-tansition="slidefade">' +
+        value.nomor +
+        ". " +
+        value.nama +
+        "</a></h6></div></div></div>"
+    );
+  });
+});
+
+let data = sessionStorage.getItem("somekey");
+
+$.getJSON("https://api-alquranid.herokuapp.com/surah/" + data, function (value) {
+  $.each(value.data, function (i, value) {
+    $("#isi-ayat").append('<div class="ayat text-center mb-5"><h5 class="fw-bolder">' + value.nomor + " " + value.ar + "</h5><i>" + value.tr + "</i><p>" + value.id + "</p></div>");
+  });
+});
